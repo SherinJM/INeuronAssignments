819975f6117199cd855c6160d1146d99cfdc332b
SherinJM <115409980+SherinJM@users.noreply.github.com>
2022-11-05 20:46:47 +0530

    Add files via upload

--- Task1.java
+++ Task1.java
@@ -1,10 +1,10 @@
-//Write a program which can store List of Integer values and print all the values using for for loop
+//Write a program which can store List of Integer values and print all the values using for loop.
 
 package assignment3;
 
 import java.util.ArrayList;
 
-public class Task2 {
+public class Task1 {
 
 	public static void main(String[] args) {
 
@@ -15,8 +15,8 @@ public class Task2 {
 		obj.add(400);
 		obj.add(500);
 
-		for (int nos : obj) {
-			System.out.println(nos);
+		for (int num = 0; num < obj.size(); num++) {
+			System.out.println(obj.get(num));
 		}
 
 	}
--- Task3.java
+++ Task3.java
@@ -1,52 +1,24 @@
-/*Task 3 – Create a program which will store students information and print the output.
-Program should ask how many students information you want to store. For each students it should ask name, email, phone, address, status
-Once all entries are done, program should ask which user information you want to fetch and should print the same.
-Note- Use scanner class for dynamic students
-*/
+//Write a program which can store List of Integer values and print all the values using for iterator
 
-package assignment2;
+package assignment3;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Task3 {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number of Student's information you wanna store");
-		int noOfStudents = sc.nextInt();
-		Object[][] array = new Object[noOfStudents][5];
+		ArrayList<Integer> obj = new ArrayList<>();
+		obj.add(100);
+		obj.add(200);
+		obj.add(300);
+		obj.add(400);
+		obj.add(500);
 
-		for (int i = 0; i < noOfStudents; i++) {
-
-			System.out.println("Enter name");
-			array[i][0] = sc.next();
-			System.out.println("Enter email");
-			array[i][1] = sc.next();
-			System.out.println("Enter phone number");
-			array[i][2] = sc.nextLong();
-			System.out.println("Enter address");
-			array[i][3] = sc.next();
-			System.out.println("Enter status");
-			array[i][4] = sc.next();
-
-		}
-
-		System.out.println("Which student information would you like to fetch ?");
-		int studentDisplay = sc.nextInt();
-
-		if (studentDisplay == 1) {
-			System.out.println("Name: " + array[0][0] + " Email: " + array[0][1] + " Phone number: " + array[0][2]
-					+ " Address: " + array[0][3] + " Status: " + array[0][4]);
-
-		} else {
-
-			System.out.println("Name: " + array[studentDisplay - 1][0] + " Email: " + array[studentDisplay - 1][1]
-					+ " Phone number: " + array[studentDisplay - 1][2] + " Address: " + array[studentDisplay - 1][3]
-					+ " Status: " + array[studentDisplay - 1][4]);
-
+		for (Iterator<Integer> i = obj.iterator(); i.hasNext();) {
+			System.out.println(i.next());
 		}
 
 	}
-
 }
